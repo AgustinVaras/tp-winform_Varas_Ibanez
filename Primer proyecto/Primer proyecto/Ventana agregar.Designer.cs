2663b6a06907dccba3f31db47ded4c243c649be1
Varas Agustin <54871540+AgustinVaras@users.noreply.github.com>
2022-09-14 11:03:41 -0300

    Add files via upload

---
+++ Primer proyecto/Primer proyecto/Ventana agregar.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace Primer_proyecto
+{
+    partial class Ventana_agregar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label_CodigoArticulo = new System.Windows.Forms.Label();
+            this.Txtbox_CodigoArticulo = new System.Windows.Forms.TextBox();
+            this.Label_Nombre = new System.Windows.Forms.Label();
+            this.Txtbox_Nombre = new System.Windows.Forms.TextBox();
+            this.Label_Precio = new System.Windows.Forms.Label();
+            this.Txtbox_Precio = new System.Windows.Forms.TextBox();
+            this.Label_Descripcion = new System.Windows.Forms.Label();
+            this.Txtbox_Descripcion = new System.Windows.Forms.TextBox();
+            this.Label_Marca = new System.Windows.Forms.Label();
+            this.Label_Categoria = new System.Windows.Forms.Label();
+            this.ComboBox_Marca = new System.Windows.Forms.ComboBox();
+            this.ComboBox_Categoria = new System.Windows.Forms.ComboBox();
+            this.Label_Imagen = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Label_CodigoArticulo
+            // 
+            this.Label_CodigoArticulo.AutoSize = true;
+            this.Label_CodigoArticulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_CodigoArticulo.Location = new System.Drawing.Point(12, 33);
+            this.Label_CodigoArticulo.Name = "Label_CodigoArticulo";
+            this.Label_CodigoArticulo.Size = new System.Drawing.Size(154, 20);
+            this.Label_CodigoArticulo.TabIndex = 0;
+            this.Label_CodigoArticulo.Text = "Codigo de articulo";
+            // 
+            // Txtbox_CodigoArticulo
+            // 
+            this.Txtbox_CodigoArticulo.Location = new System.Drawing.Point(172, 35);
+            this.Txtbox_CodigoArticulo.Name = "Txtbox_CodigoArticulo";
+            this.Txtbox_CodigoArticulo.Size = new System.Drawing.Size(156, 20);
+            this.Txtbox_CodigoArticulo.TabIndex = 1;
+            // 
+            // Label_Nombre
+            // 
+            this.Label_Nombre.AutoSize = true;
+            this.Label_Nombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_Nombre.Location = new System.Drawing.Point(39, 99);
+            this.Label_Nombre.Name = "Label_Nombre";
+            this.Label_Nombre.Size = new System.Drawing.Size(71, 20);
+            this.Label_Nombre.TabIndex = 2;
+            this.Label_Nombre.Text = "Nombre";
+            // 
+            // Txtbox_Nombre
+            // 
+            this.Txtbox_Nombre.Location = new System.Drawing.Point(172, 99);
+            this.Txtbox_Nombre.Name = "Txtbox_Nombre";
+            this.Txtbox_Nombre.Size = new System.Drawing.Size(156, 20);
+            this.Txtbox_Nombre.TabIndex = 3;
+            // 
+            // Label_Precio
+            // 
+            this.Label_Precio.AutoSize = true;
+            this.Label_Precio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_Precio.Location = new System.Drawing.Point(39, 265);
+            this.Label_Precio.Name = "Label_Precio";
+            this.Label_Precio.Size = new System.Drawing.Size(59, 20);
+            this.Label_Precio.TabIndex = 4;
+            this.Label_Precio.Text = "Precio";
+            // 
+            // Txtbox_Precio
+            // 
+            this.Txtbox_Precio.Location = new System.Drawing.Point(172, 265);
+            this.Txtbox_Precio.Name = "Txtbox_Precio";
+            this.Txtbox_Precio.Size = new System.Drawing.Size(156, 20);
+            this.Txtbox_Precio.TabIndex = 5;
+            // 
+            // Label_Descripcion
+            // 
+            this.Label_Descripcion.AutoSize = true;
+            this.Label_Descripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_Descripcion.Location = new System.Drawing.Point(402, 35);
+            this.Label_Descripcion.Name = "Label_Descripcion";
+            this.Label_Descripcion.Size = new System.Drawing.Size(103, 20);
+            this.Label_Descripcion.TabIndex = 6;
+            this.Label_Descripcion.Text = "Descripcion";
+            // 
+            // Txtbox_Descripcion
+            // 
+            this.Txtbox_Descripcion.Location = new System.Drawing.Point(522, 35);
+            this.Txtbox_Descripcion.Multiline = true;
+            this.Txtbox_Descripcion.Name = "Txtbox_Descripcion";
+            this.Txtbox_Descripcion.Size = new System.Drawing.Size(218, 92);
+            this.Txtbox_Descripcion.TabIndex = 7;
+            // 
+            // Label_Marca
+            // 
+            this.Label_Marca.AutoSize = true;
+            this.Label_Marca.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_Marca.Location = new System.Drawing.Point(39, 156);
+            this.Label_Marca.Name = "Label_Marca";
+            this.Label_Marca.Size = new System.Drawing.Size(58, 20);
+            this.Label_Marca.TabIndex = 8;
+            this.Label_Marca.Text = "Marca";
+            // 
+            // Label_Categoria
+            // 
+            this.Label_Categoria.AutoSize = true;
+            this.Label_Categoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_Categoria.Location = new System.Drawing.Point(39, 211);
+            this.Label_Categoria.Name = "Label_Categoria";
+            this.Label_Categoria.Size = new System.Drawing.Size(87, 20);
+            this.Label_Categoria.TabIndex = 9;
+            this.Label_Categoria.Text = "Categoria";
+            // 
+            // ComboBox_Marca
+            // 
+            this.ComboBox_Marca.FormattingEnabled = true;
+            this.ComboBox_Marca.Location = new System.Drawing.Point(172, 156);
+            this.ComboBox_Marca.Name = "ComboBox_Marca";
+            this.ComboBox_Marca.Size = new System.Drawing.Size(156, 21);
+            this.ComboBox_Marca.TabIndex = 10;
+            // 
+            // ComboBox_Categoria
+            // 
+            this.ComboBox_Categoria.FormattingEnabled = true;
+            this.ComboBox_Categoria.Location = new System.Drawing.Point(172, 210);
+            this.ComboBox_Categoria.Name = "ComboBox_Categoria";
+            this.ComboBox_Categoria.Size = new System.Drawing.Size(156, 21);
+            this.ComboBox_Categoria.TabIndex = 11;
+            // 
+            // Label_Imagen
+            // 
+            this.Label_Imagen.AutoSize = true;
+            this.Label_Imagen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_Imagen.Location = new System.Drawing.Point(414, 208);
+            this.Label_Imagen.Name = "Label_Imagen";
+            this.Label_Imagen.Size = new System.Drawing.Size(69, 20);
+            this.Label_Imagen.TabIndex = 12;
+            this.Label_Imagen.Text = "Imagen";
+            // 
+            // Ventana_agregar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(752, 421);
+            this.Controls.Add(this.Label_Imagen);
+            this.Controls.Add(this.ComboBox_Categoria);
+            this.Controls.Add(this.ComboBox_Marca);
+            this.Controls.Add(this.Label_Categoria);
+            this.Controls.Add(this.Label_Marca);
+            this.Controls.Add(this.Txtbox_Descripcion);
+            this.Controls.Add(this.Label_Descripcion);
+            this.Controls.Add(this.Txtbox_Precio);
+            this.Controls.Add(this.Label_Precio);
+            this.Controls.Add(this.Txtbox_Nombre);
+            this.Controls.Add(this.Label_Nombre);
+            this.Controls.Add(this.Txtbox_CodigoArticulo);
+            this.Controls.Add(this.Label_CodigoArticulo);
+            this.Name = "Ventana_agregar";
+            this.Text = "Nuevo producto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Label_CodigoArticulo;
+        private System.Windows.Forms.TextBox Txtbox_CodigoArticulo;
+        private System.Windows.Forms.Label Label_Nombre;
+        private System.Windows.Forms.TextBox Txtbox_Nombre;
+        private System.Windows.Forms.Label Label_Precio;
+        private System.Windows.Forms.TextBox Txtbox_Precio;
+        private System.Windows.Forms.Label Label_Descripcion;
+        private System.Windows.Forms.TextBox Txtbox_Descripcion;
+        private System.Windows.Forms.Label Label_Marca;
+        private System.Windows.Forms.Label Label_Categoria;
+        private System.Windows.Forms.ComboBox ComboBox_Marca;
+        private System.Windows.Forms.ComboBox ComboBox_Categoria;
+        private System.Windows.Forms.Label Label_Imagen;
+    }
+}
